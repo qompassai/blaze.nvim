16e2af7888018fda95803abf97f0f0f8de1b9ab9
Qompass Current <current@qompass.ai>
2025-05-23 18:10:24 -0700

    update

--- lua/blaze/init.lua
+++ lua/blaze/init.lua
@@ -84,26 +84,26 @@ end
 function M.setup_utils()
   require("blaze.utils").setup()
 end
-function M.setup_treesitter()
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.mojo = {
-    install_info = {
-      url = "https://github.com/qompassai/blaze-ts.nvim",
-      files = { "src/parser.c", "src/grammar.js" },
-      branch = "main",
-      generate_requires_npm = true,
-      requires_generate_from_grammar = true,
-    },
-    filetype = "mojo", "🔥"
-  }
-    require("nvim-treesitter.configs").setup {
-    ensure_installed = { "mojo", "lua", "vim", "bash", "python" },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-  }
-end
+--function M.setup_treesitter()
+--  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--  parser_config.mojo = {
+--    install_info = {
+--      url = "https://github.com/qompassai/blaze-ts.nvim",
+--      files = { "src/parser.c", "src/grammar.js" },
+--      branch = "main",
+--     generate_requires_npm = true,
+--      requires_generate_from_grammar = true,
+--    },
+--    filetype = "mojo", "🔥"
+--  }
+ --   require("nvim-treesitter.configs").setup {
+--    ensure_installed = { "mojo", "lua", "vim", "bash", "python" },
+--    highlight = {
+--      enable = true,
+--      additional_vim_regex_highlighting = false,
+--    },
+--  }
+--end
 
 local function setup_plugin(opts)
   opts = vim.tbl_deep_extend('force', config.defaults, opts or {})
